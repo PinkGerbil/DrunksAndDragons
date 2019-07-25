91addf99700823cb655dbedd952bc897041e1ad7
Unknown <zedoov3@hotmail.com>
2019-07-25 16:11:39 +1000

    changed AI script to to work with differing number of players

--- DrunksAndDragons/Assets/Scripts/AI.cs
+++ DrunksAndDragons/Assets/Scripts/AI.cs
@@ -19,17 +19,114 @@ public class AI : MonoBehaviour
     private float distance2;
     private float distance3;
 
+    private int playNum;
 
     // Start is called before the first frame update
     void Start()
     {
-       players = GameObject.FindGameObjectsWithTag("Player");
+        players = GameObject.FindGameObjectsWithTag("Player");
+        playNum = players.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(playNum == 1)
+        {
+            onePlayer();
+        }
+        if (playNum == 2)
+        {
+            twoPlayer();
+        }
+        if (playNum == 3)
+        {
+            threePlayer();
+        }
+        if (playNum == 4)
+        {
+            fourPlayer();
+        }
+    }
 
+    void attack()
+    {
+        Debug.Log("attack");
+    }
+
+    void onePlayer()
+    {
+        distance0 = Vector3.Distance(agent.transform.position, players[0].transform.position);
+
+        if (distance0 > attackRange)
+        {
+            agent.destination = players[0].transform.position;
+        }
+        else
+        {
+            attack();
+        }
+
+    }
+    void twoPlayer()
+    {
+        distance0 = Vector3.Distance(agent.transform.position, players[0].transform.position);
+        distance1 = Vector3.Distance(agent.transform.position, players[1].transform.position);
+        min1 = Mathf.Min(distance0, distance1);
+
+        if (min1 > attackRange)
+        {
+            if (min1 == distance0)
+            {
+                agent.destination = players[0].transform.position;
+            }
+            else if (min1 == distance1)
+            {
+                agent.destination = players[1].transform.position;
+            }
+        }
+        else
+        {
+            attack();
+        }
+
+
+
+    }
+    void threePlayer()
+    {
+        distance0 = Vector3.Distance(agent.transform.position, players[0].transform.position);
+        distance1 = Vector3.Distance(agent.transform.position, players[1].transform.position);
+        distance2 = Vector3.Distance(agent.transform.position, players[2].transform.position);
+
+        min1 = Mathf.Min(distance0, distance1);
+        min2 = Mathf.Min(distance2, min1);
+
+        if (min2 > attackRange)
+        {
+            if (min2 == distance0)
+            {
+                agent.destination = players[0].transform.position;
+            }
+            else if (min2 == distance1)
+            {
+                agent.destination = players[1].transform.position;
+            }
+            else if (min2 == distance2)
+            {
+                agent.destination = players[2].transform.position;
+            }
+        }
+        else
+        {
+            attack();
+        }
+
+
+
+    }
+    void fourPlayer()
+    {
         distance0 = Vector3.Distance(agent.transform.position, players[0].transform.position);
         distance1 = Vector3.Distance(agent.transform.position, players[1].transform.position);
         distance2 = Vector3.Distance(agent.transform.position, players[2].transform.position);
@@ -39,40 +136,29 @@ public class AI : MonoBehaviour
         min2 = Mathf.Min(distance2, distance3);
         min = Mathf.Min(min1, min2);
 
-        if(min > attackRange)
+        if (min > attackRange)
         {
-            chase();
+            if (min == distance0)
+            {
+                agent.destination = players[0].transform.position;
+            }
+            else if (min == distance1)
+            {
+                agent.destination = players[1].transform.position;
+            }
+            else if (min == distance2)
+            {
+                agent.destination = players[2].transform.position;
+            }
+            else if (min == distance3)
+            {
+                agent.destination = players[3].transform.position;
+            }
         }
         else
         {
             attack();
         }
 
-        
-    }
-
-    void chase()
-    {
-        if (min == distance0)
-        {
-            agent.destination = players[0].transform.position;
-        }
-        else if (min == distance1)
-        {
-            agent.destination = players[1].transform.position;
-        }
-        else if (min == distance2)
-        {
-            agent.destination = players[2].transform.position;
-        }
-        else if (min == distance3)
-        {
-            agent.destination = players[3].transform.position;
-        }
-    }
-
-    void attack()
-    {
-        Debug.Log("attack");
     }
 }
