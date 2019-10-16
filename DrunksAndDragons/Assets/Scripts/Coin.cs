aa922f2c6fa2d3d6eb7cb666bbdd9cded6fe8758
Andrew Kenny <andrewckenny97@gmail.com>
2019-10-16 15:01:02 +1100

    Changed coins to float towards nearby players

--- DrunksAndDragons/Assets/Scripts/Coin.cs
+++ DrunksAndDragons/Assets/Scripts/Coin.cs
@@ -8,9 +8,17 @@ public class Coin : MonoBehaviour
     public int yAxis;
     public int zAxis;
 
+    List<GameObject> players;
+
+    [SerializeField]
+    [Tooltip("how fast the coin moves towards the player when the player is close enough")]
+    float acceleration = 1;
+    float velocity = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        players = GameObject.Find("Game Manager").GetComponent<Blackboard>().players;
         int coinShootingX = Random.Range(-xAxis, xAxis);
         int coinShootingY = Random.Range(0, yAxis);
         int coinShootingZ = Random.Range(-zAxis, zAxis);
@@ -21,8 +29,28 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 closest = Vector3.positiveInfinity;
+
+        foreach(GameObject child in players)
+        {
+            if (Vector3.Distance(child.transform.position, transform.position) < Vector3.Distance(closest, transform.position))
+                closest = child.transform.position;
+        }
+        closest.y = transform.position.y;
+        if (Vector3.Distance(transform.position, closest) < 3)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            velocity += acceleration * Time.deltaTime;
+            transform.position += (closest - transform.position).normalized * velocity * Time.deltaTime;
+        }
+        else
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            velocity = 0;
+        }
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
