b4c01eaa07620fd148fcd29661cb3d818b5f896d
zack <zedoov@hotmail.com>
2019-10-31 14:13:23 +1100

    changed coins so they cant be picked up while dead

--- DrunksAndDragons/Assets/Scripts/Coin.cs
+++ DrunksAndDragons/Assets/Scripts/Coin.cs
@@ -60,7 +60,7 @@ public class Coin : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "Player" && other.gameObject.GetComponent<PlayerDamageHandler>().Alive)
         {
             other.GetComponent<PlayerPoints>().AddPoints(1);
             Destroy(this.gameObject);
